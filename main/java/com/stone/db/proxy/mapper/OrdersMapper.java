46440461f279ead4c38d31582e5be7253ac723da
StonyShi <faith789510@gmail.com>
2016-01-13 15:00:37 +0800

    增加mybatis自动生成配置

---
+++ main/java/com/stone/db/proxy/mapper/OrdersMapper.java
@@ -0,0 +1,17 @@
+package com.stone.db.proxy.mapper;
+
+import com.stone.db.proxy.model.Orders;
+
+public interface OrdersMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
---
+++ main/java/com/stone/db/proxy/model/Orders.java
@@ -0,0 +1,35 @@
+package com.stone.db.proxy.model;
+
+import java.util.Date;
+
+public class Orders {
+    private Integer id;
+
+    private String orderNo;
+
+    private Date createDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo == null ? null : orderNo.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
