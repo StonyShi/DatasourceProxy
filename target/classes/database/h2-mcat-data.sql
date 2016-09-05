104ca08a6e2c8741920f6bcd1ccfd03728683496
StonyShi <faith789510@gmail.com>
2016-09-05 11:17:59 +0800

    删除目录target

--- target/classes/database/h2-mcat-data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO cat(id,name,create_date) values(1,'Lue',now());
-INSERT INTO cat(id,name,create_date) values(2,'Book',now());
-INSERT INTO cat(id,name,create_date) values(3,'Yu',now());
-INSERT INTO cat(id,name,create_date) values(4,'Kei',now());
-INSERT INTO cat(id,name,create_date) values(5,'Lup',now());
-INSERT INTO cat(id,name,create_date) values(6,'You',now());
-INSERT INTO cat(id,name,create_date) values(7,'Bel',now());
--- target/classes/database/h2-mcat.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS cat (
-    id int not null IDENTITY,
-    name varchar(80),
-    create_date TIMESTAMP,
-    constraint pk_cat_id primary key (id)
-);
